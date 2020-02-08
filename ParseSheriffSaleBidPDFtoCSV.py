77940647a85e61c720fbda97400105ebaebdef04
edyoung75 <edyoung75ny@gmail.com>
2020-02-08 09:41:29 -0500

    Parsing of PDF into a list. Currently not able to create a CSV at this time. Will need to work on sorting through the plaintiff name and sort through through the list.

--- ParseSheriffSaleBidPDFtoCSV.py
+++ ParseSheriffSaleBidPDFtoCSV.py
@@ -8,6 +8,22 @@ import csv
 import PyPDF4 as pdf
 import ParseSheriffSaleBidPDFtoCSVFunctions as fn
 
+csvHeader = ['Docket #',
+             'Attorney Name',
+             'Plaintiff Name',
+             'Sale Type',
+             'Sale Date',
+             'CostTax',
+             'Cost',
+             'Sale Status',
+             'PP Date',
+             'Reason for PP',
+             'Svs',
+             '3129',
+             'OK',
+             'AuctionNo',
+             'Defendant Name']
+
 content = fn.getPDFContent('./data/bid_list.pdf')
 # print the content
 print(content)
--- ParseSheriffSaleBidPDFtoCSVFunctions.py
+++ ParseSheriffSaleBidPDFtoCSVFunctions.py
@@ -1,9 +1,9 @@
 import PyPDF4 as pdf
 import re
 
-docNumPattern = r'^[a-zA-Z]'
+docNumPattern = re.compile(r'\w{2}-\w{2}-\w{6}')
+endOfPagePattern = re.compile('^Report.*')
 
-#r'(^[a-zA-Z][a-zA-Z]\-[0-9][0-9]\-.*)'
 def getPDFContent(path):
     content = ""
     file = open(path, "rb")
@@ -16,16 +16,68 @@ def getPDFContent(path):
 
         content += tContent + "\n"
 
-        if re.match(re.compile(docNumPattern, flags=re.IGNORECASE),content):
-            print("++++ found you ++++")
-        else:
-            print("-------------------- did not match ------------------")
     #content = " ".join(content.replace(u"\xa0", " ").strip().split())
     return content
 
 def parsePageContent(pageContent):
     content = ""
     lineCount = pageContent.count('\n')
-    content = pageContent
     print(lineCount)
+    print("\n================ Content ===============\n")
+    chunks = pageContent.split('\n')
+    print(chunks)
+    docketNum = ""
+    docketContentList = []
+    docketDictionary = {}
+    docketListCount = 0
+    docketPlaintiffName = []
+    for col in chunks:
+        if docNumPattern.match(col):
+            # Clear any content that isn't associated with a docket
+            # Also set the
+            if docketNum == "":
+                docketContentList = []
+                docketNum = col
+                docketListCount += 1
+
+            # If another docket # is found and it's not empty, then the system must have found all the elements of the docket
+            # Update the docketDictionary with all the new docket information.
+            else:
+                docketDictionary.update({docketNum : docketContentList})
+                docketContentList = []
+                docketNum = col
+            print("found a Docket #" + docketNum)
+        elif endOfPagePattern.match(col):
+            # Update the dictionary
+            docketDictionary.update({docketNum : docketContentList})
+            # Hit the last item on the page so wipe all existing content
+            docketNum = ""
+            docketContentList = []
+            docketDictionary = {}
+            #print("hit the end of the page " + col)
+            break
+        else:
+            docketContentList.append(col)
+            #print("ADDING to the Content List ")
+            print(docketContentList)
+
+    print(docketDictionary)
+    content = pageContent
+
     return content
+
+
+# Function checks if the string
+# contains any special character
+def run(string):
+    # Make own character set and pass
+    # this as argument in compile method
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
+    # Pass the string in search
+    # method of regex object.
+    if (regex.search(string) == None):
+        print("String is accepted")
+
+    else:
+        print("String is not accepted.")
