bf5a7c68f08b52713de365939a4045715ba50a61
edyoung75 <edyoung75ny@gmail.com>
2020-02-09 09:17:10 -0500

    Narrative: "Fixed" POSTPONED issue. A column for the Postponed till date would follow POSTPONED. Now the script will combine POSTPONED with the Date. However, maybe I need to create columns for "Sale Status" that are not POSTPONED instead. Will need to work on "Comment" as well to combine all the comments at the end. In addition, will need to add functionality to clean up 'Svs' '3129' 'OK'
    TODO:
    1. Document code with Comments.
    2. Solve truncating data at the end of the page.
    3. Evaluate each docket for automatic clean up.
    4. Clean up the code a little.
    5. Should add columns instead of combining them for these:
    'Sale Status' 'PP Date' 'Reason for PP' 'Svs' '3129' 'OK'

--- ParseSheriffSaleBidPDFtoCSVFunctions.py
+++ ParseSheriffSaleBidPDFtoCSVFunctions.py
@@ -5,12 +5,28 @@ docNumPattern = re.compile(r'\w{2}-\w{2}-\w{6}')
 endOfPagePattern = re.compile('^Report.*')
 possibleSaleTypesHas2ndLine = 0
 possibleSaleTypesWith2ndLine = ['Sci Fa sur Tax ',
-                     'Mortgage ',
-                     'Other Real ']
+                                'Mortgage ',
+                                'Other Real ']
+
 possibleSaleTypes2ndLine = ['Lien',
                             'Foreclosure',
                             'Estate']
 possibleSaleTypesOnly1Line = ['Municipal Lien']
+ignoreList = ['Docket #',
+             'Attorney Name',
+             'Plaintiff Name',
+             'Sale Type',
+             'Sale Date',
+             'CostTax',
+             'Cost',
+             'Sale Status',
+             'PP Date',
+             'Reason for PP',
+             'Svs',
+             '3129',
+             'OK',
+             'AuctionNo',
+             'Defendant Name']
 
 # Function returns a list of dockets and contents organized into another list
 def getPDFContent(path):
@@ -34,6 +50,9 @@ def getPDFContent(path):
     # Return the final Master list
     return contentList
 
+docketNum = ""
+docketContentList = []
+
 def parsePageContent(pageContent):
     content = ""
     lineCount = pageContent.count('\n')
@@ -41,11 +60,16 @@ def parsePageContent(pageContent):
     print("\n================ Content ===============\n")
     chunks = pageContent.split('\n')
     #print(chunks)
-    docketNum = ""
-    docketContentList = []
+
+    global docketNum
+    global docketContentList
+
     docketInitialContents = []
     docketListCount = 0
     tempContentStr = ""
+
+    tPostponeTrigger = ""
+    tCommentTrigger = ""
 
     for col in chunks:
         # find the docket #
@@ -65,6 +89,7 @@ def parsePageContent(pageContent):
                 #add the previous docket to the docketDictionary
                 #docketDictionary.update({docketNum : docketContentList})
                 docketInitialContents.append(docketContentList)
+                print(docketContentList)
                 docketContentList = []
                 docketNum = col
                 docketContentList.append(col)
@@ -76,10 +101,13 @@ def parsePageContent(pageContent):
         elif col in possibleSaleTypesOnly1Line:
             docketContentList.append(tempContentStr)
             docketContentList.append(col)
+            tempContentStr = ""
             docketListCount += 1
         elif col in possibleSaleTypesWith2ndLine:
             docketContentList.append(tempContentStr)
             tempContentStr = col
+            print(tempContentStr)
+            print(docketListCount)
         elif col in possibleSaleTypes2ndLine:
             tempContentStr = tempContentStr + col
             docketContentList.append(tempContentStr)
@@ -96,19 +124,48 @@ def parsePageContent(pageContent):
             #docketDictionary.update({docketNum : docketContentList})
             docketInitialContents.append(docketContentList)
             # Hit the last item on the page so wipe all existing content
-            docketNum = ""
-            docketContentList = []
-            docketDictionary = {}
+            #docketNum = ""
+            #docketContentList = []
+            #docketDictionary = {}
             tempContentStr = ""
             #print("hit the end of the page " + col)
-            docketListCount = 0
+            #docketListCount = 0
             break
+        elif col in ignoreList:
+            # Do nothing with the info
+            print("Ignoring "+col)
+        elif tPostponeTrigger:
+            docketContentList.append(tPostponeTrigger+" "+col)
+            tPostponeTrigger = ""
+ #           print("[[[[[[[temp]]]]]]]")
+ #           print(tempContentStr)
+ #           docketContentList.append(tempContentStr+" "+col)
+ #           tempContentStr = ""
+ #           docketListCount += 1
+        elif col == "POSTPONED":
+            tPostponeTrigger = col
+
+        elif tCommentTrigger and col == chunks[-1]:
+            #tCommentTrigger = tCommentTrigger + " " + col
+            #docketContentList.append(tCommentTrigger)
+            #tCommentTrigger = ""
+        elif tCommentTrigger:
+            #tCommentTrigger = tCommentTrigger + " " + col
+        elif col == "Comments:" and col == chunks[-1]:
+            #tCommentTrigger = col
+            #docketContentList.append(tCommentTrigger)
+            #tCommentTrigger = ""
+        elif col == "Comments:":
+            tCommentTrigger = col
         else:
             docketContentList.append(col)
             docketListCount += 1
             #print("ADDING to the Content List ")
             #print(docketContentList)
 
+    print(docketContentList)
+    if not docketContentList:
+        docketInitialContents.append(docketContentList)
     #print(docketInitialContents)
 
     return docketInitialContents
